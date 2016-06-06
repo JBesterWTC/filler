1b42d917429153739725f3f4cb7a3cb6526f318f
Joshua BESTER <jbester@e5r10p9.jnb.42.fr>
2016-06-06 13:18:04 +0200

    filler.h updated

--- filler.h
+++ filler.h
@@ -1,8 +1,10 @@
 #ifndef FILLER_H
 # define FILLER_H
-
 # include <string.h>
+# include <fcntl.h>
 # include "libft/includes/libft.h"
+# define PLAYER1 1
+# define PLAYER2 2
 
 typedef struct  s_board
 {
@@ -10,5 +12,15 @@ typedef struct  s_board
 	int     y;
 	char    **map;
 }               t_board;
+
+typedef struct	s_players
+{
+	int		x1;
+	int		x2;
+	int		opx;
+	int		opy;
+	char	**opp;
+	char	**me;
+}				t_players;
 
 #endif
