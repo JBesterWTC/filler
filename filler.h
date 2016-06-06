2a7aa356f149567d6c91d92ae542e6c10b020927
Tony MACK <tmack@e5r9p10.jnb.42.fr>
2016-06-06 12:46:11 +0200

    tma

---
+++ filler.h
@@ -0,0 +1,16 @@
+#ifndef FILLER_H
+# define FILLER_H
+
+# include "libft/includes/libft.h"
+
+# define PLAYER1  1
+# define PLAYER2  2
+
+typedef struct s_board
+{
+	int		x;
+	int		y;
+	char	**map;
+}				t_board;
+
+#endif
--- main.c
+++ main.c
@@ -6,10 +6,11 @@
 /*   By: tmack <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/06 10:50:16 by tmack             #+#    #+#             */
-/*   Updated: 2016/06/06 10:54:57 by tmack            ###   ########.fr       */
+/*   Updated: 2016/06/06 12:45:04 by tmack            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft\includes\libft.h"
 #include "filler.h"
 
 int main(int argc, char **argv)
@@ -20,4 +21,11 @@ int main(int argc, char **argv)
 	int		i = 0;
 
 	fd = open(argv[0], O_RDONLY);
+
+	while (ft_get_next_line(fd, &lines))
+	{
+		ft_putstr(lines[i]);
+		ft_putchar('\n');
+		i++;
+	}
 }
