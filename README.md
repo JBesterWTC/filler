8e263160b371d892c6f7677d87e497e1d3f156a4
Joshua BESTER <jbester@e5r10p9.jnb.42.fr>
2016-06-06 12:50:53 +0200

    First commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# filler
-Filler player AI for WeThinkCode_SA
-Developed by kgani, tmack, jbester.
--- filler.h
+++ filler.h
@@ -1,7 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   filler.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbester <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/06/06 12:23:37 by jbester           #+#    #+#             */
+/*   Updated: 2016/06/06 12:48:45 by jbester          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FILLER_H
 # define FILLER_H
-
-#include <stdlib.h>
+# define PLAYER1 1
+# define PLAYER2 2
+# include <stdlib.h>
+# include <string.h>
+# include "libft.h"
 
 typedef struct	s_board
 {
@@ -9,5 +24,15 @@ typedef struct	s_board
 	int		y;
 	char	**map;
 }				t_board;
+
+typedef struct	s_players
+{
+	int		x1;
+	int		x2;
+	int		opx;
+	int		opy;
+	char	**opp;
+	char	**self;
+}				t_players;
 
 #endif
